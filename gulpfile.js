21eaa5ebb56afdb0828dd71aa1d9bbce0c2c76cb
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-17 19:12:14 -0500

    local serv task

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,24 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     fs = require('fs'),
     tap = require('gulp-tap');
+
+// Notes: Run a Local Server Setup - 
+// gulp.task('server', function() {
+//   gulp.src('src')
+//     .pipe(server({
+//       livereload: true
+//     }));
+// });
+
+//Run the Local Server
+gulp.task('server', function () {
+    var server = gls('./src/app.js', {
+        env: {}
+    });
+    server.start();
+
+    // Reload Server / Change
+    gulp.watch(['./gulpfile.js', './src/app.js'], function () {
+        server.start.bind(server)()
+    });
+});
