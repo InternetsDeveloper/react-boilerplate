b304f52ed7bf7c01838848f27f9be8a3bac52fc3
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-17 19:40:57 -0500

    dependencies

---
+++ src/public/jsx/todo.js
@@ -0,0 +1,9 @@
+// Require dependencies
+// --------------------
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    request = require('superagent'),
+    _ = require('lodash');
+
+
+ReactDOM.render( < div > Hello < /div>, document.getElementById('component'));
